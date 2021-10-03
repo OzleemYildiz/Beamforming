2d8118a0ff09446d3c40bff1e0aeacb19a0fdf39
Ozlem Yildiz <zy2043@log-1.nyu.cluster>
2021-10-03 04:52:30 -0400

    run_tree.sh

--- run_tree.sh
+++ run_tree.sh
@@ -2,16 +2,16 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=6:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem=8GB
-#SBATCH --job-name=MatlabJobName
+#SBATCH --job-name=tree
 #SBATCH --mail-type=END
-#SBATCH --mail-user=YOURNETID@nyu.edu
-#SBATCH --output=slurm_out/MatlabJobName.out
-#SBATCH --error=slurm_out/MatlabJobName.err
+#SBATCH --mail-user=zy2043@nyu.edu
+#SBATCH --output=out_tree_cont.out
+#SBATCH --error=tree_cont.err
 
 module purge
 module load matlab/2020b
-cd /scratch/zy2043/beamforming
+cd /scratch/zy2043/beamforming/Beamforming
 cat combine_aykin_tree.m | srun matlab -nodisplay
 
