b1d0e06d242f35f088827f43cfd072b18816bace
OzleemYildiz <zlmyldz96@gmail.com>
2021-10-07 01:18:08 -0400

    hwan

--- freq_Hwang.m
+++ freq_Hwang.m
@@ -1,11 +1,15 @@
  % What if we use HWANG in 2 frequency with each having 2^{alpha} size
 
 function [beam_loc, n_steps] = freq_Hwang(n,m, valid_loc, beam_loc, location, n_steps)
-    if n == m
-        %Append the locations to check
-        %But no need to check but I know that all of them are paths
-        beam_loc = [beam_loc, valid_loc];
-        return;
+%     if n == m
+%         %Append the locations to check
+%         %But no need to check but I know that all of them are paths
+%         beam_loc = [beam_loc, valid_loc];
+%         return;
+%     end
+    
+    if n == 0
+        return
     end
     
     if m == 0
@@ -33,36 +37,67 @@ function [beam_loc, n_steps] = freq_Hwang(n,m, valid_loc, beam_loc, location, n_
         check1 = sum(location(valid_loc(1: size)) == 0)== size;
         
         %NACK for the second part of a size of 2^alpha
-        check2 = sum(location(valid_loc(size+1: min(2*size, end))) == 0)== length(valid_loc(size+1: min(2*size, end)));
+        hold = length(valid_loc(size+1: min(2*size, end)));
+        check2 = sum(location(valid_loc(size+1: min(2*size, end))) == 0)== hold;
         n_steps = n_steps + 1;
         
         
-        if check1 && check2 == 0
+        if size == 1 
+            if check1 == 0 && check2 == 0 && hold ~= 0
+                beam_loc = [beam_loc, valid_loc(1: 2*size)];
+                valid_loc = valid_loc(2*size+1: end);
+                n = n-2;
+                m = m-2;
+            elseif check1 == 0 && check2  && hold ~= 0
+                beam_loc = [beam_loc, valid_loc(1: size)];
+                valid_loc = valid_loc(2*size+1: end);
+                n = n-2;
+                m = m-1;
+            elseif check1 && check2 ==0  && hold ~= 0
+                beam_loc = [beam_loc, valid_loc(size+1: 2*size)];
+                valid_loc = valid_loc(2*size+1: end);
+                n = n-2;
+                m = m-1;
+            elseif check1 == 0
+                beam_loc = [beam_loc, valid_loc(1: size)];
+                valid_loc = valid_loc(size+1: end);
+                n = n-1;
+                m = m-1;
+            else
+                valid_loc = valid_loc(size+hold+1: end);
+                n = n-size-hold;
+            end
+            
+            [beam_loc, n_steps] = freq_Hwang(n,m, valid_loc, beam_loc, location, n_steps); 
+
+        end
+            
+        
+        if check1 && check2 == 0 && size~=1
             valid_loc = valid_loc(size+1: end);
             n = n-size;
-        elseif check2 && check1 == 0
+        elseif check2 && check1 == 0 && size~=1
             n = n - length(valid_loc(size+1: min(2*size, end)));
             valid_loc = [valid_loc(1:size), valid_loc(min(2*size, end)+1 :end)];
-        elseif check1 && check2
+        elseif check1 && check2 && size~=1
             n= n - length(valid_loc(1: min(2*size, end)));
             valid_loc = valid_loc(min(2*size, end)+1:end);
-           
         end
         
         
-        if check1 || check2
+        if check1 || check2 && size~=1
             [beam_loc, n_steps] = freq_Hwang(n,m, valid_loc, beam_loc, location, n_steps); 
             
-        elseif check1 == 0 && check2 == 0 % Both side has 1
+        elseif check1 == 0 && check2 == 0 && size~=1 % Both side has 1
             %Parallel Binary Splitting
                         
             [beam_loc, test_n1, n,m, valid_loc_1] = binary_split(n, m, location, valid_loc(1:size),size, beam_loc, 0);
-            [beam_loc, test_n2, n,m, valid_loc_2] = binary_split(n, m, location, valid_loc(size+1:end),size, beam_loc, 0);
+            [beam_loc, test_n2, n,m, valid_loc_2] = binary_split(n, m, location, valid_loc(size+1:min(2*size,end)),hold, beam_loc, 0);
             
             %Note that I count one more in binary split. I check the same
             %size again
             n_steps = n_steps + max(test_n1, test_n2)-1;
-            valid_loc = [valid_loc_1, valid_loc_2];
+            valid_loc = [valid_loc_1, valid_loc_2,  valid_loc(min(2*size +1, end):end)];
             
             [beam_loc, n_steps] = freq_Hwang(n,m, valid_loc, beam_loc, location, n_steps); 
        
