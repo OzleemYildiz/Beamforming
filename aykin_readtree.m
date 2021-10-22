8efc3777eb91906ca17756ecd010d33dfd1138d9
OzleemYildiz <zlmyldz96@gmail.com>
2021-10-22 02:35:24 -0400

    aykinnoisy

--- aykin_readtree.m
+++ aykin_readtree.m
@@ -1,34 +1,55 @@
 
-%L = [8:26, 28:35];
-%P = 2;
+L = 8:35;
+P = 2;
 
-trial =100000;
+% L = [8:27, 30];
+% P = 3;
 
-L = [8:26, 30];
-P = 3;
-
-%L = [8:24];
-%P = 4;
-
-
+% L = [8:24, 26];
+% P = 4;
 
 lpb = b_optimize(P,L);
 
+num_step_mlbs = zeros(4, length(lpb)); 
+pmd = [0, 0.39,0.19, 0.12];
+pfa = [0,0.05,0.19, 0.4]; 
 
-num_step_mlbs = zeros(1, length(lpb)); 
+trial =5000000;
+constant = length(lpb);
 
-for j = 1:length(lpb)
-    tree_aykin = load("/scratch/zy2043/beamforming/Beamforming/tree/aykin_tree_L" + lpb(j,1)  + "P" + lpb(j,2) + "B" + lpb(j,3) + ".mat");
-    for i=1:trial  
-        location = [ones(1,lpb(j,2)), zeros(1,lpb(j,1)-lpb(j,2))];
-        location = location(randperm(length(location))); 
+blockage_mlbs = zeros(length(lpb),4);
+md_mlbs = zeros(length(lpb),4);
 
-        [res, num_s] = tree_trace(location, tree_aykin, 0);
-        num_step_mlbs(j) = num_step_mlbs(j) + num_s;
+for k=1:4
+    for j = 1:constant
+    %     tree_aykin = load("/scratch/zy2043/beamforming/Beamforming/tree/aykin_tree_L" + lpb(j,1)  + "P" + lpb(j,2) + "B" + lpb(j,3) + ".mat");
+        tree_aykin = load("/Users/ozlemyildiz/Desktop/Beamforming_ICC/Beamforming/tree/aykin_tree_L" + lpb(j,1)  + "P" + lpb(j,2) + "B" + lpb(j,3) + ".mat");
+
+        for i=1:trial  
+            location = [ones(1,lpb(j,2)), zeros(1,lpb(j,1)-lpb(j,2))];
+            location = location(randperm(length(location))); 
+
+            [res, num_s] = tree_trace(location, tree_aykin, 0, pmd(k), pfa(k));
+            num_step_mlbs(j) = num_step_mlbs(j) + num_s;
+            true_loc = find(location == 1 );
+            
+            if isempty(res)
+                 blockage_mlbs(j,k) = blockage_mlbs(j,k) +1;
+            else   
+                 if sum(true_loc == res', 'all') == 0
+                     blockage_mlbs(j,k) = blockage_mlbs(j,k) +1;
+                 end
+                 if sum(true_loc == res', 'all') ~= P
+                     md_mlbs(j,k) = md_mlbs(j,k) + abs(P - sum(true_loc == res', 'all'));
+                 end
+            end
+        end
     end
 end
 num_step_mlbs = num_step_mlbs./trial;
+md_mlbs = md_mlbs./trial;
+blockage_mlbs = blockage_mlbs./trial;
 
-save('num_step_aykin_3', 'num_step_mlbs');
-save('b_optimize3', 'lpb')
-
+save("num_step_aykinP" +P , 'num_step_mlbs');
+save("md_mlbsP" +P , 'md_mlbs');
+save("blockage_mlbsP" +P , 'blockage_mlbs');
--- tree_trace.m
+++ tree_trace.m
@@ -1,18 +1,37 @@
-function [res, num_s] = tree_trace(location, tree_aykin, num_step)
+function [res, num_s] = tree_trace(location, tree_aykin, num_step, pmd, pfa)
 
     num_s = num_step;
     if isfield(tree_aykin, 'root')
         num_s = num_s +1;
         if isfield(tree_aykin.root, 'root')  
             root = tree_aykin.root.root;
-            if sum (location(root)== 1) >0 % I got 1-> Left
-                [res, num_s] = tree_trace(location, tree_aykin.left, num_s);
+            check = sum (location(root)== 1) >0; %If ACK, its 1
+            
+            pe = rand(1,1);
+            if check == 1 && pe< pmd % random error satisfies, it's not ACK anymore 
+                check = 0;
+            elseif  check == 0 && pe< pfa
+                check = 1;
+            end
+            
+            if check % I got 1-> Left
+                [res, num_s] = tree_trace(location, tree_aykin.left, num_s, pmd,pfa);
             else % I got 0 -> Right
-                [res, num_s] = tree_trace(location, tree_aykin.right, num_s);
+                [res, num_s] = tree_trace(location, tree_aykin.right, num_s, pmd,pfa);
             end
         else
             root = tree_aykin.root;
-            if sum (location(root)== 1) >0 % I got 1-> Left
+            
+            check = sum (location(root)== 1) >0; %If ACK, its 1
+            
+            pe = rand(1,1);
+            if check == 1 && pe< pmd % random error satisfies, it's not ACK anymore 
+                check = 0;
+            elseif  check == 0 && pe< pfa
+                check = 1;
+            end
+            
+            if check % I got 1-> Left
                 res = tree_aykin.leftchild;
             else % I got 0 -> Right
                 res = tree_aykin.rightchild;
