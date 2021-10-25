8c55d7e141b2feb07aa3392edd3889fc96fb39f2
Ozlem Yildiz <zy2043@log-1.nyu.cluster>
2021-10-25 13:19:09 -0400

    new

--- run_gt.sh
+++ run_gt.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=72:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --time=140:00:00
 #SBATCH --mem=8GB
-#SBATCH --job-name=gt_f_1
+#SBATCH --job-name=gt_fix
 #SBATCH --mail-type=END
 #SBATCH --mail-user=zy2043@nyu.edu
-#SBATCH --output=gt_f_1.out
-#SBATCH --error=gt_f_1.err
+#SBATCH --output=gt_fix.out
+#SBATCH --error=gt_fix.err
 
 module purge
 module load matlab/2020b
 cd /scratch/zy2043/beamforming/Beamforming
-cat Group_Testing.m | srun matlab -nodisplay
+cat GT_prob.m | srun matlab -nodisplay
 
