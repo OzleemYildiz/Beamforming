53a32dc4de1d9bc21c901fed846b2a101afc8b1d
OzleemYildiz <zlmyldz96@gmail.com>
2022-04-11 23:48:05 -0400

    MultiLevel

---
+++ 5GSimulations/energy_average_detect.m
@@ -0,0 +1,89 @@
+
+% Compare the energy level detection for 1 AoA in one angular interval and
+% 2 AoA in one angular interval
+
+% In order to justify multi-level, we need to demonstrate that the energy
+% level helps us to determine
+
+% I will check for only hybrid exhaustive
+method =1;
+N = 8:64;
+m = 4; %defective, %clusters
+threshold= 12;
+
+%Multilevel initalization
+count_2paths = zeros(length(N),1);
+count_1path = zeros(length(N),1);
+snr_2paths = zeros(length(N),1);
+snr_1path = zeros(length(N),1);
+
+
+trial = 100;
+
+%Since this simulation is for multilevel measuring
+multilevel =1;
+
+for k = 1:length(N)
+%     resolution = (2*pi/N(k));
+%     N_antenna=  round(2*0.89/resolution);
+%     beam_locs = 0:resolution:pi;
+    
+    %total_codebook = N(k)/4; %search over pi/2
+
+    
+    for j = 1:length(m)        
+        for i = 1:length(threshold)
+            for tr = 1:trial  
+                total_codebook = N(k);
+                location = zeros(1, N(k));
+                %Because of ULA antenna array, angles of user is limited to be
+                %in [0, pi]. I should decide allocations and make changes to
+                %search only in this region. 
+               
+                [gain_gaussian, angle_ue] = channel(m(j), multilevel); 
+
+
+                %Where are the locations of the angles
+                %I compare with the beams ending locations and sum to find the
+                %index
+
+                %true_loc = sum(angle_ue' > beam_locs,2)';
+                %true_loc = locate_AoA_index(angle_ue, N);
+                true_loc = sum(angle_ue' > linspace(0, 2*pi, N(k)+1), 2)';
+
+                
+                % Again create a location array for indeces in beamforming 
+                location(true_loc)=1;
+                
+   
+                [beam_loc, n_steps, snr_2paths_i, snr_1path_i, count_2paths_i,count_1path_i ] = exhaustive_hybrid_5g(total_codebook, m(j), location, gain_gaussian, angle_ue, threshold(i));
+                    
+                count_2paths(k) = count_2paths(k)+count_2paths_i;
+                count_1path(k) = count_1path(k)+count_1path_i;
+                snr_2paths(k) = snr_2paths(k)+snr_2paths_i;
+                snr_1path(k) = snr_1path(k)+snr_1path_i;
+
+                res_hex = cell2mat(beam_loc);
+                res_hex = unique(res_hex); 
+                
+                if length(res_hex) > m(j)
+                     %fprintf('error\n')
+                end
+                
+                
+
+            end
+
+        end
+
+    end
+end
+
+
+snr_1path = snr_1path./count_1path;
+snr_2paths = snr_2paths./count_2paths;
+
+
+% save('5gsimulation_ntest_hex_03_08_sectored', 'number_of_test_hex')
+% save('5gsimulation_blockage_hex_03_08_sectored', 'blockage')
+% save('5gsimulation_md_hex_03_08_sectored', 'md')
--- 5GSimulations/exhaustive_hybrid_5g.m
+++ 5GSimulations/exhaustive_hybrid_5g.m
@@ -2,7 +2,14 @@
 %function pathexists = beamform(total_codebook, bf_index,gain_gaussian, angle_ue,threshold)
 %indeces will be checked through beamform function instead of putting yes.
 
-function [beam_loc, n_steps] = exhaustive_hybrid_5g(n, m, location, gain_gaussian, angle_ue, threshold)
+function [beam_loc, n_steps, snr_2paths, snr_1path, count_2paths,count_1path] = exhaustive_hybrid_5g(n, m, location, gain_gaussian, angle_ue, threshold)
+    %I nitalization for multi-level
+    count_2paths = 0;
+    snr_2paths = 0;
+    snr_1path = 0;
+    count_1path = 0;
+    
+    
     beam_loc = {};
     n_steps= 0;
     for ex = 1:2:n
@@ -20,7 +27,7 @@ function [beam_loc, n_steps] = exhaustive_hybrid_5g(n, m, location, gain_gaussia
         %check_ex_1 = location(ex) == 0; % =0 ,ACK
         
         %Perform beamforming and =1 is ACK
-        pathexists_1 = beamform_sectored(n,n, ex,gain_gaussian, angle_ue, threshold);
+        [pathexists_1, snr_2paths_1, snr_1path_1, count_2paths_1, count_1path_1] = beamform_sectored(n,n, ex,gain_gaussian, angle_ue, threshold);
 
 
         if pathexists_1 %ACK
@@ -30,14 +37,26 @@ function [beam_loc, n_steps] = exhaustive_hybrid_5g(n, m, location, gain_gaussia
         
         if ex+1 <= n
             %check_ex_2 = location(ex+1) == 0; % =0 ,ACK
-            pathexists_2 = beamform_sectored(n,n, ex+1, gain_gaussian, angle_ue, threshold);
+            [pathexists_2, snr_2paths_2, snr_1path_2, count_2paths_2,count_1path_2] = beamform_sectored(n,n, ex+1, gain_gaussian, angle_ue, threshold);
 
             %ACK
             if pathexists_2 && m ~=0
                 beam_loc = [beam_loc, ex+1];
                 m = m-1;
             end
+            
+            %Addition for Multi-level measuring
+            count_2paths = count_2paths + count_2paths_1 + count_2paths_2;
+            count_1path = count_1path + count_1path_1 + count_1path_2;
+            snr_2paths = snr_2paths + snr_2paths_1 + snr_2paths_2;
+            snr_1path = snr_1path+ snr_1path_1 + snr_1path_2;
+        else
+            count_1path = count_1path + count_1path_1;
+            count_2paths = count_2paths+ count_2paths_1;
+            snr_2paths = snr_2paths +snr_2paths_1;
+            snr_1path = snr_1path +snr_1path_1;
         end
+               
 
     end
 end
